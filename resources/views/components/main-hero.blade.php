8a0c2319aef2f691cc2d1476137bed87dad2c031
bagasysf <bagasysf@gmail.com>
2022-04-14 23:37:00 +0700

    selesai membuat main hero responsive all device

--- resources/views/components/main-hero.blade.php
+++ resources/views/components/main-hero.blade.php
@@ -1,9 +1,15 @@
-<div class="flex flex-col">
-    <div class="absolute z-20 flex flex-col h-full w-full justify-end text-center pb-32 text-xl text-white space-y-5">
-        <p class="font-cinzel">The Wedding Of</p>
-        <p class="font-august text-4xl font-bold">Angga & Vivi</p>
-        <p class="font-cinzel">Kamis, 26 May 2022</p>
+<div class="flex flex-col lg:flex-row lg:h-screen overflow-hidden">
+    <div class="flex flex-col h-screen w-full lg:w-6/12 overflow-hidden">
+        <div class="absolute lg:w-6/12 z-20 flex flex-col h-full w-full justify-end text-center pb-28 text-white space-y-5 md:space-y-7">
+            <p class="font-cinzel font-bold text-xl md:text-2xl lg:text-3xl">The Wedding Of</p>
+            <p class="font-august text-4xl md:text-6xl font-bold">Angga & Vivi</p>
+            <p class="font-cinzel font-bold text-xl md:text-2xl lg:text-3xl pt-3 lg:pt-5">Kamis, 26 May 2022</p>
+        </div>
+        <img src="/images/main-hero-gradient.png" alt="main-hero-gradient" class="absolute z-10 h-screen w-full lg:w-6/12 object-cover">
+        <img src="/images/main-hero-mobile.png" alt="cover-image-mobile" class="h-screen object-cover">
     </div>
-    <img src="/images/main-hero-gradient.png" alt="main-hero-gradient" class="absolute z-10 h-screen w-full">
-    <img src="/images/main-hero-mobile.png" alt="cover-image-mobile" class="h-screen w-full">
+    <div class="flex flex-col lg:w-6/12 lg:justify-center font-cinzel text-center md:text-lg py-28 px-12 md:px-24 lg:px-48 space-y-3 text-stone-500 bg-stone-300">
+        <p class="text-sm md:text-lg lg:text-2xl">“Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan diantaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.”</p>
+        <p class="text-base md:text-xl lg:text-3xl font-bold">( QS Ar - Rum 21 )</p>
+    </div>
 </div>
